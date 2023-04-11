13784195a239301b18ffbad22da465dc4188e0db
Draoui Bilal <bilaldraoui22@gmail.com>
2023-04-11 22:11:22 +0200

    Add enviorement variables type safety

---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export const env = [
+    'BOT_TOKEN',
+    'NODE_ENV'
+]
---
+++ src/utils/checkENV.ts
@@ -0,0 +1,10 @@
+import { env } from "../config/env";
+
+export const checkENV = () => {
+
+    const missing = env.filter((envVar) => !process.env[envVar]);
+
+    if (missing.length > 0) {
+        throw new Error(`Missing environment variables: ${missing.join(', ')}`);
+    }
+}
