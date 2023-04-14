9c782ed527ec020fb32cbd1071f571c4776b8648
lanjriw <walid.lanjri@grenoble-inp.org>
2023-04-14 01:15:59 +0100

    Welcome new members

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,2 +1,3 @@
 export * from "./readyHandler";
 export * from "./messages/answerMessageHandler";
+export * from "./messages/welcomeMessageHandler";
---
+++ src/handlers/messages/welcomeMessageHandler.ts
@@ -0,0 +1,16 @@
+import { type channel } from "diagnostics_channel";
+import {type GuildMember, type Client, TextChannel } from "discord.js";
+
+export const welcomeMessage = (client:Client)=>async (memeber:GuildMember)=>{
+
+    //fetch the general channel by it ID
+    let general: TextChannel | null = null;
+
+    client.channels.cache.forEach((channel) => {
+        if(channel instanceof TextChannel && channel.name === "general") {
+            general = channel;
+            general.send(`welcome ${memeber.user} be careful drale is sudo`);
+        }
+    });
+
+}
